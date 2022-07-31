c9daf78af4624fbf51f2a41c904b15d1e72a20e5
subwayminder <subwayminder@gmail.com>
2022-07-31 17:33:08 +0300

    init

--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::group(['middleware' => 'auth:sanctum'], function () {
+
+});
+
 Route::post('register', [AuthController::class, 'register']);
 Route::post('token', [AuthController::class, 'getToken']);
